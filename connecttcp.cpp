9615ba0a80b3449519a6f142a96360558a693008
Az-pro-afk <truonghuuhuy0402@gmail.com>
2024-10-30 21:19:21 +0700

    Update connecttcp.cpp

--- connecttcp.cpp
+++ connecttcp.cpp
@@ -5,7 +5,6 @@ connectTCP::connectTCP(QObject *parent)
     : QThread(parent), running(false), host("127.0.0.1"), port(12001)
 {
 }
-
 connectTCP::~connectTCP()
 {
     stop();
@@ -15,47 +14,37 @@ connectTCP::~connectTCP()
         delete tcpSocket;
     }
 }
-
 void connectTCP::setPixmap(const QPixmap &pixmap)
 {
     pixmapToSend = pixmap;
 }
-
 void connectTCP::stop()
 {
     running = false;
-
-    // Dừng kết nối TCP một cách an toàn từ cùng luồng
     if (tcpSocket && tcpSocket->isOpen()) {
         QMetaObject::invokeMethod(tcpSocket, "close", Qt::QueuedConnection);
     }
 
-    quit();  // Kết thúc vòng lặp sự kiện của luồng nếu có
-    wait();  // Chờ cho luồng kết thúc hẳn
+    quit();
+    wait();
     tcpSocket->close();
 }
-
 void connectTCP::run()
 {
     running = true;
     tcpSocket = new QTcpSocket();
-
-    // Connect to the host
     tcpSocket->connectToHost(host, port);
-
     if (!tcpSocket->waitForConnected(3000)) {
         emit errorOccurred("Could not connect to server");
         running = false;
         return;
     }
-
     emit connectionStatusChanged(true);
-
     while (running) {
         if (!pixmapToSend.isNull()) {
             sendPixmap();
         }
-        QThread::msleep(120);  // Adjust sleep duration as necessary
+        QThread::msleep(120); 
     }
 
     emit connectionStatusChanged(false);
@@ -69,17 +58,14 @@ void connectTCP::sendPixmap()
         emit errorOccurred("Failed to convert QPixmap to PNG");
         return;
     }
-
     qint64 bytesWritten = tcpSocket->write(pngData);
 
     if (bytesWritten == -1) {
         emit errorOccurred("Failed to send QPixmap");
         return;
     }
-
     tcpSocket->flush();
 }
-
 QByteArray connectTCP::convertPixmapToPNG(const QPixmap &pixmap)
 {
     QByteArray byteArray;
@@ -89,6 +75,5 @@ QByteArray connectTCP::convertPixmapToPNG(const QPixmap &pixmap)
     if (!pixmap.save(&buffer, "PNG")) {
         emit errorOccurred("Failed to convert QPixmap to PNG");
     }
-
     return byteArray;
 }
